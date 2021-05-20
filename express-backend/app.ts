22979823064002a52674106279e47a16e3855d11
Maria Ricci <mery.ricci.mr@gmail.com>
2021-05-20 14:22:35 +0200

    added eslint, airbnb styleguide in the backend

--- express-backend/app.ts
+++ express-backend/app.ts
@@ -1,44 +1,42 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
-const app = express()
 
-const port = 3000
+const app = express();
+
+const port = 3000;
 const sitename = 'https://github.com/cap-ricci/weather-app/0.1';
 const corsOptions = {
-    origin: 'http://localhost:8080',
-  }
+  origin: 'http://localhost:8080',
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/data', function (req, res) { 
-  return res.send('Received a GET HTTP method');   
-  
-});
+app.get('/data', (req, res) => res.send('Received a GET HTTP method'));
 
-app.post('/data', function (req, res) {
+app.post('/data', (req, res) => {
   const coord = {
-    lat : req.body.lat,
-    lon : req.body.lon
-  }
-  const url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat='+coord.lat+'&lon='+coord.lon;
+    lat: req.body.lat,
+    lon: req.body.lon,
+  };
+  const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${coord.lat}&lon=${coord.lon}`;
   fetch(url, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'User-Agent': sitename,
-        },
-      })
-    .then(res => {
-      console.log(res.headers)
-      res.json()
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'User-Agent': sitename,
+    },
+  })
+    .then((externalRes) => {
+      console.log(externalRes.headers);
+      res.json();
     })
-    .then(json => res.send(json))
+    .then((json) => res.send(json));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
